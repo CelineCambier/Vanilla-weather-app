3ab977ce1630ff2586993928b8b4bfa0b858aa87
CelineCambier <cambier.celine2@gmail.com>
2023-05-08 20:38:53 -0700

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -14,23 +14,26 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response){
-    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.time * 1000);
+    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`); 
+    iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "e48e1aab0acf4fc215c13f02ct8bo5ab";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New-York&key=e48e1aab0acf4fc215c13f02ct8bo5ab&units=metric`;
+let city = "New-York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=e48e1aab0acf4fc215c13f02ct8bo5ab&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
